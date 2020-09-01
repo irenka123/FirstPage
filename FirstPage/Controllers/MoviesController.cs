fc8d559002eac56bc55ac12dbdb3579b405ba66a
irenka123 <iryna.s@wp.pl>
2020-09-01 23:58:02 +0200

    Added FirstProject

---
+++ FirstPage/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using FirstPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstPage.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random   
+        public ActionResult Random()    
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+                   return View(movie);
+            /* return Content("Hello!");
+             return new EmptyResult();
+             return HttpNotFound();*/
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+    }
+}
