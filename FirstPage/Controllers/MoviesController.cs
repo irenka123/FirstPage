aa04932b86e5d30f46b3b979abe111c7e5f25ec1
irenka123 <iryna.s@wp.pl>
2020-09-11 06:29:37 +0200

    redirect to new page

--- FirstPage/Controllers/MoviesController.cs
+++ FirstPage/Controllers/MoviesController.cs
@@ -9,6 +9,7 @@ namespace FirstPage.Controllers
 {
     public class MoviesController : Controller
     {
+       
         // GET: Movies/Random   
         public ActionResult Random()    
         {
@@ -19,5 +20,25 @@ namespace FirstPage.Controllers
              return HttpNotFound();*/
             //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+            
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        }
+
     }
 }
